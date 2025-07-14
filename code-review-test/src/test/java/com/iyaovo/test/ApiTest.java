1779fa1c97ce4494efd2ec384de0c7b85e4bb9fe
iyaovo <2293257241@qq,com>
2025-07-14 22:22:37 +0800

    feat：代码评审功能测试v2

--- code-review-test/src/test/java/com/iyaovo/test/ApiTest.java
+++ code-review-test/src/test/java/com/iyaovo/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaa1233131"));
+        System.out.println(Integer.parseInt("aaaaaaa"));
     }
 
 }
